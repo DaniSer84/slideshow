3a886386a66a2b543c6ff51ed932546a953270f4
DaniSer84 <serenelli84@gmail.com>
2024-07-27 21:52:50 +0200

    functions moved in function.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 import { SLIDES } from "./modules/slides.js";
-import { createSlideShow } from "./modules/functions.js";
+import { createSlideShow, showSlides, currentSlide, plusSlides, slideIndex } from "./modules/functions.js";
 
 // VARIABLES
 let slideshowContainer = document.querySelector('.slideshow-container')
 let dotsContainer = document.getElementById('dots-container')
-let slideIndex = 0;
 let prev = document.querySelector('.prev') 
 let next = document.querySelector('.next')
 let timeout
@@ -12,12 +11,13 @@ let timeout
 //EVENTS AND FUNCTIONS
 createSlideShow(slideshowContainer, dotsContainer, SLIDES)
 
+repeatedPlusSlides()
+
+
 function repeatedPlusSlides() {
   plusSlides(1)
   timeout = setTimeout(repeatedPlusSlides, 2000)
 }
-
-repeatedPlusSlides()
 
 let slidesAndDotsContainer = document.querySelector('.slides-and-dots-container')
 
@@ -25,7 +25,6 @@ slidesAndDotsContainer.onmouseenter = () => clearTimeout(timeout)
 slidesAndDotsContainer.onmouseleave = () => repeatedPlusSlides()
 
 prev.addEventListener('click', () => {
-  console.log(slideIndex)
   plusSlides(-1)
 })
 next.addEventListener('click', () => {
@@ -33,14 +32,9 @@ next.addEventListener('click', () => {
   plusSlides(1)
 })
 
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+showSlides();
 
 let dots = document.getElementsByClassName('dot')
-console.log(dots)
 let dotsList = Array.from(dots)
 dotsList.forEach(dot => {
   dot.addEventListener('click', () => {
@@ -48,27 +42,5 @@ dotsList.forEach(dot => {
   })
 })
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {
-    slideIndex = 1
-  }    
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
