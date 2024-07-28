64257676aac380ca2ab0089e51d339ef8d496439
DaniSer84 <serenelli84@gmail.com>
2024-07-28 08:48:33 +0200

    added gradient, changed font and spaces

--- modules/slides.js
+++ modules/slides.js
@@ -3,19 +3,19 @@ const SLIDES = [
         id: 1, 
         title: 'Title prima slide',
         description: 'Description slide Description slideDescription slideDescription slide',
-        imageSrc: 'https://loremflickr.com/320/240/rome'
+        imageSrc: 'https://placehold.co/600x400/png?text=slide1'
     },
     {
         id: 2, 
         title: 'Title seconda slide',
         description: 'Description slide  slideDescription slide',
-        imageSrc: 'https://loremflickr.com/320/240/venice'
+        imageSrc: 'https://placehold.co/600x400/png?text=slide2'
     },
     {
         id: 3, 
         title: 'Title terza slide',
         description: 'Description slide Description slideDescription slideDescription  slideDescription slideslideDescription slideslideDescription slide',
-        imageSrc: 'https://loremflickr.com/320/240/milan'
+        imageSrc: 'https://placehold.co/600x400/png?text=slide3'
     }
 ]
 
